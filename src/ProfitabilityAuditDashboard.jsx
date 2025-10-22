54c0653d86144b4eabd15ebd08752e81d2c01465
Mike Scharnhorst <scharnhorstm@yahoo.com>
2025-10-22 17:24:36 -0400

    Update API URL to new deployment

--- src/ProfitabilityAuditDashboard.jsx
+++ src/ProfitabilityAuditDashboard.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { DollarSign, TrendingDown, TrendingUp, Target, AlertCircle, CheckCircle2, Printer, Download } from 'lucide-react';
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwq3YZrEi3WxsaIv5PuBAEPOGXSCWvXlrN4JjNIaJ2l0lkIIa_dY97fgEJGECyO-HuJ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxO6lmAQfW8hLvxYqCY_9HSBIHmlNkvCykLRVDk-DbVbBX4AmGzVwP1_hPWXw6cMjc/exec';
+
 
 const ProfitabilityAuditDashboard = () => {
   const [data, setData] = useState(null);
